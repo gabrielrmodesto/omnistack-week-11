a0042074bb5dfdec0b98dab3ef32d0965741952c
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-03-28 19:04:10 -0300

    Frontend finish

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,10 +1,37 @@
 import React from "react";
-import logoIMG from '../../assets/logo.svg';
+import logoIMG from "../../assets/logo.svg";
 import "./style.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { useState } from "react";
+import api from "../../services/api";
 
 export default function NewIncident() {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [value, setValue] = useState("");
+	const ongId = localStorage.getItem("ongID");
+	const history = useHistory();
+
+	async function handleNewIncident(event) {
+		event.preventDefault();
+		const data = {
+			title,
+			description,
+			value
+		};
+
+		try {
+			await api.post("/incidents", data, {
+				headers: {
+					Authorization: ongId,
+				}
+			});
+			history.push('/profile');
+		} catch (error) {
+			alert("Erro ao cadastrar");
+		}
+	}
 	return (
 		<div className="new-incident-container">
 			<div className="content">
@@ -12,18 +39,33 @@ export default function NewIncident() {
 					<img src={logoIMG} alt="Be the hero" />
 					<h1>Register a new case</h1>
 					<p>
-						Describe your case with details to find a hero to resolve this
+						Describe your case with details to find a hero to
+						resolve this
 					</p>
 					<Link className="back-link" to="/profile">
 						<FiArrowLeft size={16} color="#E02042" />
 						Home
 					</Link>
 				</section>
-				<form action="">
-					<input type="text" placeholder="Title" />
-					<textarea placeholder="Description" />
-					<input type="text" placeholder="Value R$" />
-					
+				<form onSubmit={handleNewIncident}>
+					<input
+						type="text"
+						placeholder="Title"
+						value={title}
+						onChange={event => setTitle(event.target.value)}
+					/>
+					<textarea
+						placeholder="Description"
+						value={description}
+						onChange={event => setDescription(event.target.value)}
+					/>
+					<input
+						type="text"
+						placeholder="Value R$"
+						value={value}
+						onChange={event => setValue(event.target.value)}
+					/>
+
 					<button type="submit" className="button">
 						Register
 					</button>
