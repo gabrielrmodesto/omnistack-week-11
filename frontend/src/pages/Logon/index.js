38bbb03cbb2e46aa617aab742ca658a75b498d69
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-03-28 13:40:01 -0300

    Login page style

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiLogIn } from 'react-icons/fi';
 import "./style.css";
 import heroesIMG from '../../assets/heroes.png'
 import logoIMG from '../../assets/logo.svg';
@@ -11,8 +12,9 @@ export default function Logon() {
 				<form action="">
 					<h1>Make the logon</h1>
 					<input type="text" placeholder="Your ID"/>
-					<button type="submit">Enter</button>
+					<button className="button" type="submit">Enter</button>
 					<a href="/register">
+						<FiLogIn size={16} color="#E02042" />
 						Don't have a register
 					</a> 
 				</form>
